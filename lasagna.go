04d0e16d1b9826a797c9675b2acda36bec760c1f
Mayank Sinha <codemacs@users.noreply.github.com>
2023-03-12 20:10:01 -0400

    Update lasagna.go

--- lasagna.go
+++ lasagna.go
@@ -1,6 +1,9 @@
 package lasagna
 
-var OvenTime int = 40
+const (
+    OvenTime = 40
+    minsTakenPerLayer = 2
+) 
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
@@ -9,11 +12,11 @@ func RemainingOvenTime(actualMinutesInOven int) int {
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	return numberOfLayers*2 
+	return numberOfLayers*minsTakenPerLayer
 }
 
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers int, actualMinutesInOven int ) int {
-    return (numberOfLayers*2 + actualMinutesInOven)
+    return ( PreparationTime(numberOfLayers) + actualMinutesInOven)
 }
